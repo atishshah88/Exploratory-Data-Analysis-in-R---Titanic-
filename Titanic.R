270c2026396f7ec4fe7d488634eaa945aa84299c
atishshah88 <atishshah88@gmail.com>
2017-08-19 23:28:46 -0700

    Add files via upload

---
+++ Titanic.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(ggplot2)
+data(Titanic)
+titanic <- data.frame(Titanic)
+
+
+# 1 - Check the structure of titanic
+str(titanic)
+
+# 2 - Use ggplot() for the first instruction
+ggplot(titanic, aes(x = Class, fill = Sex)) + geom_bar(position = "dodge")
+
+# 3 - Plot 2, add facet_grid() layer
+ggplot(titanic, aes(x = Class, fill = Sex)) + geom_bar(position = "dodge") + facet_grid(.~Survived)
+
+
+# 4 - Define an object for position jitterdodge, to use below
+posn.jd <- position_jitterdodge(0.5, 0, 0.6)
+
+# 5 - Plot 3, but use the position object from instruction 4
+ggplot(titanic, aes(x = Class, y= Age, col = Sex)) + geom_point(size = 3, alpha = 0.5, position = posn.jd) + facet_grid(.~Survived)
